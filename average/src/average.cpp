853ae41bef64b73010648a54a12a7ccfa51bd534
Hi <Hi@DESKTOP-C9439GR>
2021-07-20 16:20:02 +0530

    average
    uploading eclipes sample files

---
+++ average/src/average.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : average.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	if(num<0){
+	printf("Number is negative");
+
+	}else{
+		printf("Number is 	Positive");
+	}
+
+	return EXIT_SUCCESS;
+}
